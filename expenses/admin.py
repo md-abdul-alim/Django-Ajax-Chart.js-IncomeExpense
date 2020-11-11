f1d0d44ca4ddd8a120b55ad15480758ae56d47c5
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-11-11 22:51:05 +0600

    Admin panel modification

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Expense, Category
 # Register your models here.
-admin.site.register(Expense)
+
+class ExpenseAdmin(admin.ModelAdmin):
+
+    list_display = ('owner','category','description','amount','date',)
+    search_fields = ('category','description','amount','date',)
+admin.site.register(Expense,ExpenseAdmin)
 admin.site.register(Category)
